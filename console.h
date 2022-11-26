a6c5d03f8f0286095f8baaf1220783d193a59395
mingtiancai <2427373908@qq.com>
2022-11-26 19:58:54 +0800

    ENH: add mouse event

--- console.h
+++ console.h
@@ -3,12 +3,16 @@
 
 #include <QPlainTextEdit>
 
-class Console : public QPlainTextEdit
-{
-    Q_OBJECT
-public:
-    explicit Console(QWidget *parent = nullptr);
+class Console : public QPlainTextEdit {
+  Q_OBJECT
+ public:
+  explicit Console(QWidget *parent = nullptr);
+
+ protected:
+  void keyPressEvent(QKeyEvent *e) override;
+  void mousePressEvent(QMouseEvent *e) override;
+  void mouseDoubleClickEvent(QMouseEvent *e) override;
+  void contextMenuEvent(QContextMenuEvent *e) override;
 };
 
-
-#endif // CONSOLE_H
+#endif  // CONSOLE_H
